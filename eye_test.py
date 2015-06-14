a76ddf7b2043205ef730197518d24f11aaffcf50
sufian002 <sufian.latif.bd@gmail.com>
2015-06-15 01:09:38 +0600

    Get centers of the cells.

--- eye_test.py
+++ eye_test.py
@@ -7,7 +7,8 @@ import pyscreenshot
 import time
 from PIL import Image
 
-def countGridSize(image):
+
+def getGridSize(image):
     assert isinstance(image, PIL.Image.Image)
     width = image.size[0]
     tmp = 10
@@ -16,20 +17,26 @@ def countGridSize(image):
     while i < width:
         while i < width and image.getpixel((i, tmp)) != (255, 255, 255):
             i += 1
-
         size += 1
-
         while i < width and image.getpixel((i, tmp)) == (255, 255, 255):
             i += 1
-
     return size
+
+
+def getCellCenters(image):
+    n = getGridSize(image)
+    gap = 4
+    cellSize = int((image.size[0] - (n - 1) * gap) / n)
+    stops = [(2 * i + 1) * cellSize / 2 + i * gap for i in range(n)]
+    print stops
+    points = [[(x, y) for x in stops] for y in stops]
+    return points
 
 url = 'https://www.igame.com/eye-test/?fbs=32'
 
-webbrowser.open_new(url)
-time.sleep(10)
+# webbrowser.open_new(url)
+# time.sleep(10)
 
-im=pyscreenshot.grab(bbox = (647, 286, 983, 622))
-#im = Image.open('7.png')
-gridSize = countGridSize(im)
-print gridSize
+# im=pyscreenshot.grab(bbox = (647, 286, 983, 622))
+im = Image.open('7.png')
+getCellCenters(im)
